b396b1ee8950d35cfaf324c1b6777031e8e4c068
zhenjia xu <zhenjia xu>
2016-09-04 22:58:35 +0800

    leetcode algorithms problem 3

---
+++ algorithms/3_length_of_longest_substring.cc
@@ -0,0 +1,33 @@
+nclude <stdio.h>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Solution {
+    public:
+        int lengthOfLongestSubstring(string s) {
+            unordered_map<char, int> letter_2_idx;
+
+            int max_len = 0;
+            int idx = 0;
+            for (auto &c : s)
+            {
+                ++idx;
+                if (letter_2_idx.count(c) > 0)
+                {
+                    max_len = std::max(max_len, idx - letter_2_idx[c]);
+                }
+                letter_2_idx[c] = idx;
+            }
+
+            return max_len;
+        }
+};
+
+int main ()
+{
+    string s{"pwwkew"};
+    printf("%d\n", Solution().lengthOfLongestSubstring(s));
+    return 0;
+}
+
