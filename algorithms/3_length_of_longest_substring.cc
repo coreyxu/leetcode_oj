d74113d96ccd0d93a630f33ea079d24bab728533
corey <xuzhj007@gmail.com>
2016-09-05 20:41:52 +0800

    fix problem 3 bug

--- algorithms/3_length_of_longest_substring.cc
+++ algorithms/3_length_of_longest_substring.cc
@@ -10,15 +10,19 @@ class Solution {
 
             int max_len = 0;
             int idx = 0;
+            int base_idx = 1;
             for (auto &c : s)
             {
                 ++idx;
-                if (letter_2_idx.count(c) > 0)
+                if (letter_2_idx.count(c) > 0 && letter_2_idx[c] >= base_idx)
                 {
-                    max_len = std::max(max_len, idx - letter_2_idx[c]);
+                    max_len = std::max(max_len, idx - base_idx);
+                    base_idx = letter_2_idx[c] + 1;
                 }
                 letter_2_idx[c] = idx;
             }
+            ++idx;
+            max_len = std::max(max_len, idx - base_idx);
 
             return max_len;
         }
